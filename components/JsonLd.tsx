0228137f76eb9d803b9afed10ad1ea0a9389894e
honeyliepatricio <honeyliepatriciorn@gmail.com>
2026-09-06 17:35:49 +0800

    Remove duplicate root folders

--- components/JsonLd.tsx
+++
@@ -1,9 +0,0 @@
-export default function JsonLd({ data }: { data: Record<string, unknown> }) {
-  return (
-    <script
-      type="application/ld+json"
-      // eslint-disable-next-line react/no-danger
-      dangerouslySetInnerHTML={{ __html: JSON.stringify(data) }}
-    />
-  );
-}
